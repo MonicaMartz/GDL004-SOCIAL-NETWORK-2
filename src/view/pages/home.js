3d10986f3a745113f2f0f50667c1eb3df83743e3
MonicaMartz <monik92_ms@hotmail.com>
2020-04-16 00:33:27 -0500

    primera versión de pagina de inicio con css

--- src/view/pages/home.js
+++ src/view/pages/home.js
@@ -1,19 +1,22 @@
 export default () => {
     const homePage = `
-    <main class="main-container"></main>
-    <div class = "content">    
-
-    <div class="logo">
-        <h1 class="text-logo">Plant Lovers</h1>
-        <img src="/src/images/plantLoverCute.jpg" alt="Logo"  width="50%" height="70%">
-    </div>
-        <h2 class="title f-open">Bienvenidx</h1>
-        <h3 class="f-open sub-title">Slogan sobre plantas</h3>
-
-        <button class="f-open cta-start"><a href="#/signup">Sign up</a></button>
-        <button class="f-open cta-start"><a href="#/signin">Sign In</a></button>
+    <div class="main-container"></main>
+     <div class = "content">    
+        <div class="logo">
+            <h2 class="text-logo">Plant Lover</h2>
+            <h4 class="slogan">Comunidad que florece</h4>
+            <img src="/src/images/logo.png" alt="Logo"  width="50%" ">
+        </div>
+        <div class ="welcome">
+        <h3 class="titlen">Bienvenido</h3>
+        
+        </div>
+        <button class="button-Sign"><a href="#/signup">Sign up</a></button>
+        <button class="button-SignUp"><a href="#/signin">Sign In</a></button>
    
     </div>
+    <div class="footer">Copyrigth 2020 Mónica MartSosa</div>
+    </main>
     `
 
     const divHome = document.createElement("div");
